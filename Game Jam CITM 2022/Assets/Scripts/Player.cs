9801a7a6392e2225c163523851b096fd860175eb
rastabrandy02 <73582888+rastabrandy02@users.noreply.github.com>
2022-02-02 21:00:34 +0100

    Shooting

--- Game Jam CITM 2022/Assets/Scripts/Player.cs
+++ Game Jam CITM 2022/Assets/Scripts/Player.cs
@@ -2,11 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum lookingAt
+{
+    RIGHT,
+    LEFT
+};
 public class Player : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public GameObject proyectile;
     public Vector2 speed = new Vector2 (5,5);
     public bool canJump = true;
+
+    lookingAt orientation = lookingAt.RIGHT;
+    
+    
+   
     
     void Start()
     {
@@ -21,19 +32,39 @@ public class Player : MonoBehaviour
         if (Input.GetKey("d"))
         {
             rb.velocity = new Vector2(speed.x,rb.velocity.y);
+            orientation = lookingAt.RIGHT;
         }
         if (Input.GetKey("a"))
         {
             rb.velocity = new Vector2(-speed.x, rb.velocity.y);
+            orientation = lookingAt.LEFT;
         }
         if (Input.GetKey("space") && canJump)
         {
             rb.velocity = new Vector2(rb.velocity.x, speed.y);
             canJump = false;
         }
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(orientation == lookingAt.RIGHT)
+            {
+                Instantiate(proyectile, new Vector2(gameObject.transform.position.x + 2, gameObject.transform.position.y), Quaternion.identity);
+            }
+           if(orientation == lookingAt.LEFT)
+            {
+                Instantiate(proyectile, new Vector2(gameObject.transform.position.x - 2, gameObject.transform.position.y), Quaternion.identity);
+            }
+            
+        }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground") canJump = true;
+    }
+    public Vector2 GetPosition()
+    {
+        Vector2 ret = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+        
+        return ret;
     }
 }
---
+++ Game Jam CITM 2022/Assets/Scripts/proyectile.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class proyectile : MonoBehaviour
+{
+    private GameObject player;
+    private Vector2 speed;
+    public int secondsToDestroy = 3;
+    private Rigidbody2D rb;
+    private float deathTimer;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        deathTimer = 0;
+        //Vector2 playerPos = player.transform.position;
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        if (gameObject.transform.position.x > player.transform.position.x)
+        {
+            speed = new Vector2(10, 0);
+        }
+        if(gameObject.transform.position.x < player.transform.position.x)
+        {
+            speed = new Vector2(-10, 0);
+        }
+    }
+
+    
+    void FixedUpdate()
+    {
+        deathTimer += Time.deltaTime;
+        rb.velocity = speed;
+        if(deathTimer >= secondsToDestroy)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
